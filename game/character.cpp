ce26244194d8815566cf703d51af0a5b6b149e84
vijay senapathi <vijaysenapathi@gmail.com>
2013-04-16 14:39:30 +0530

    started coding rigging of the character

--- game/character.cpp
+++ game/character.cpp
@@ -1,7 +1,43 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+#include <GL/glu.h>
+#include <GL/freeglut.h>
+
+//pi/180
+#define pi180 0.017453293
+
 class character(){
+	//l length of leg
+	//a length from diaphram to pelvis
+	//b length from neck till diaphram
+	//c distance between legs
+	//h length of hand
+	private float l,a,b,c,h;
+
+	//all angles are capital letters
+	//A angle between head and the body
+	//B1  
+	//B2
+	//D1
+	//D2
+	//E1
+	//E2
+	//G1
+	//G2
+	//T
+	private int A,B1,B2,D1,D2,E1,E2,G1,G2,T;
+
+	character(){
+
+	}
+
+	draw(){
+		glPushMatrix();
+		glTranslate3f();
+		glPopMatrix();
+	}
+
 
 
 
