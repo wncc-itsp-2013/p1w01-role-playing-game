4d6966a48ec11b2edc26c1225d351d10e23c36de
vijay senapathi <vijaysenapathi@gmail.com>
2013-04-27 15:59:50 +0530

    sdl works well now in windowed too

--- game/main.cpp
+++ game/main.cpp
@@ -10,27 +10,7 @@
 
 float rotate_x=0;
 float rotate_y=0;
-bool running;
-static GLboolean should_rotate = GL_TRUE;
 static int quitSDL(int code);
-
-void keyboardKeys(unsigned char key,int x,int y){
-	switch(key){
-		case 'w':
-			rotate_x-=5;
-			break;
-		case 's':
-			rotate_x+=5;
-			break;
-		case 'a':
-			rotate_y+=5;
-			break;
-		case 'd':
-			rotate_y-=5;
-			break;
-	}
-	//glutPostRedisplay();
-}
 
 static void handle_key_down(SDL_keysym* keysym){
 	switch(keysym->sym){
@@ -38,7 +18,6 @@ static void handle_key_down(SDL_keysym* keysym){
 			quitSDL(0);
 			break;
 		case SDLK_SPACE:
-			should_rotate = !should_rotate;
 			break;
 		case SDLK_UP:
 			rotate_x-=5;
@@ -113,33 +92,6 @@ void initializeGL(int w, int h)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void initializeGL1(int width, int height){
-
-float ratio = (float) width / (float) height;
-/* Our shading model--Gouraud (smooth). */
-glShadeModel( GL_SMOOTH );
-/* Culling. */
-glCullFace( GL_BACK );
-glFrontFace( GL_CCW );
-glEnable( GL_CULL_FACE );
-/* Set the clear color. */
-glClearColor( 1, 0, 0, 0 );
-/* Setup our viewport. */
-glViewport( 0, 0, width, height );
-/*
- * * Change to the projection matrix and set
- * * our viewing volume.
- * */
-glMatrixMode( GL_PROJECTION );
-glLoadIdentity( );
-/*
- * * EXERCISE:
- * * Replace this with a call to glFrustum.
- * */
-gluPerspective( 60.0, ratio, 1.0, 1024.0 );
-
-}
-
 void draw_screen(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -164,146 +116,6 @@ void draw_screen(void)
 	glFlush();
 
 	SDL_GL_SwapBuffers();
-}
-
-void draw_screen1(void)
-{
-/* Our angle of rotation. */
-static float angle = 0.0f;
-/*
-* EXERCISE:
-* Replace this awful mess with vertex
-* arrays and a call to glDrawElements.
-*
-* EXERCISE:
-* After completing the above, change
-* it to use compiled vertex arrays.
-*
-* EXERCISE:
-* Verify my windings are correct here ;).
-*/
-static GLfloat v0[] = { -1.0f, -1.0f, 1.0f };
-static GLfloat v1[] = { 1.0f, -1.0f, 1.0f };
-static GLfloat v2[] = { 1.0f, 1.0f, 1.0f };
-static GLfloat v3[] = { -1.0f, 1.0f, 1.0f };
-static GLfloat v4[] = { -1.0f, -1.0f, -1.0f };
-static GLfloat v5[] = { 1.0f, -1.0f, -1.0f };
-static GLfloat v6[] = { 1.0f, 1.0f, -1.0f };
-static GLfloat v7[] = { -1.0f, 1.0f, -1.0f };
-static GLubyte red[] = { 255, 0, 0, 255 };
-static GLubyte green[] = { 0, 255, 0, 255 };
-static GLubyte blue[] = { 0, 0, 255, 255 };
-static GLubyte white[] = { 255, 255, 255, 255 };
-static GLubyte yellow[] = { 0, 255, 255, 255 };
-static GLubyte black[] = { 0, 0, 0, 255 };
-static GLubyte orange[] = { 255, 255, 0, 255 };
-static GLubyte purple[] = { 255, 0, 255, 0 };
-/* Clear the color and depth buffers. */
-glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-/* We don’t want to modify the projection matrix. */
-glMatrixMode( GL_MODELVIEW );
-glLoadIdentity( );
-/* Move down the z-axis. */
-glTranslatef( 0.0, 0.0, -5.0 );
-/* Rotate. */
-glRotatef( angle, 0.0, 1.0, 0.0 );
-if( should_rotate ) {
-if( ++angle > 360.0f ) {
-angle = 0.0f;
-}
-}
-/* Send our triangle data to the pipeline. */
-glBegin( GL_TRIANGLES );
-glColor4ubv( red );
-glVertex3fv( v0 );
-glColor4ubv( green );
-glVertex3fv( v1 );
-glColor4ubv( blue );
-glVertex3fv( v2 );
-glColor4ubv( red );
-glVertex3fv( v0 );
-glColor4ubv( blue );
-glVertex3fv( v2 );
-glColor4ubv( white );
-glVertex3fv( v3 );
-glColor4ubv( green );
-glVertex3fv( v1 );
-glColor4ubv( black );
-glVertex3fv( v5 );
-glColor4ubv( orange );
-glVertex3fv( v6 );
-glColor4ubv( green );
-glVertex3fv( v1 );
-glColor4ubv( orange );
-glVertex3fv( v6 );
-glColor4ubv( blue );
-glVertex3fv( v2 );
-glColor4ubv( black );
-glVertex3fv( v5 );
-glColor4ubv( yellow );
-glVertex3fv( v4 );
-glColor4ubv( purple );
-glVertex3fv( v7 );
-glColor4ubv( black );
-glVertex3fv( v5 );
-glColor4ubv( purple );
-glVertex3fv( v7 );
-glColor4ubv( orange );
-glVertex3fv( v6 );
-glColor4ubv( yellow );
-glVertex3fv( v4 );
-glColor4ubv( red );
-glVertex3fv( v0 );
-glColor4ubv( white );
-glVertex3fv( v3 );
-glColor4ubv( yellow );
-glVertex3fv( v4 );
-glColor4ubv( white );
-glVertex3fv( v3 );
-glColor4ubv( purple );
-glVertex3fv( v7 );
-glColor4ubv( white );
-glVertex3fv( v3 );
-glColor4ubv( blue );
-glVertex3fv( v2 );
-glColor4ubv( orange );
-glVertex3fv( v6 );
-glColor4ubv( white );
-glVertex3fv( v3 );
-glColor4ubv( orange );
-glVertex3fv( v6 );
-glColor4ubv( purple );
-glVertex3fv( v7 );
-glColor4ubv( green );
-glVertex3fv( v1 );
-glColor4ubv( red );
-glVertex3fv( v0 );
-glColor4ubv( yellow );
-glVertex3fv( v4 );
-glColor4ubv( green );
-glVertex3fv( v1 );
-glColor4ubv( yellow );
-glVertex3fv( v4 );
-glColor4ubv( black );
-glVertex3fv( v5 );
-glEnd( );
-/*
-* EXERCISE:
-* Draw text telling the user that ’Spc’
-* pauses the rotation and ’Esc’ quits.
-* Do it using vetors and textured quads.
-*/
-/*
-* Swap the buffers. This this tells the driver to
-* render the next frame from the contents of the
-* back-buffer, and to set all rendering operations
-* to occur on what was the front-buffer.
-*
-* Double buffering prevents nasty visual tearing
-* from the application drawing on areas of the
-* screen that are being updated at the same time.
-*/
-SDL_GL_SwapBuffers( );
 }
 
 void reshape(int w, int h){
@@ -351,8 +163,8 @@ void initializeSDL(){
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,16);	
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 
-	flags = SDL_OPENGL | SDL_FULLSCREEN;
-	//flags = SDL_SWSURFACE | SDL_ANYFORMAT;
+	//flags = SDL_OPENGL | SDL_FULLSCREEN;
+	flags = SDL_OPENGL | SDL_ANYFORMAT;
 
 	if(SDL_SetVideoMode(width, height,bpp, flags)==0){
 		std::cout<<"Video mode set failed"<<std::endl;
@@ -371,15 +183,6 @@ int main(int argc, char** argv)
 		draw_screen();
 
 	}
-	//glutInit(&argc, argv);
-	//glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	//glutInitWindowSize(1000, 700);
-	//glutInitWindowPosition(100, 100);
-	//glutCreateWindow("Game");
-	//glutDisplayFunc(display);
-	//glutReshapeFunc(reshape);
-	//glutKeyboardFunc(keyboardKeys);
-	//glutMainLoop();
 	return 0;
 }
 
